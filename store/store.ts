86c3d6ab042d3a7d9490138dfff961234ae4bfa4
Ronak Parmar <parmarronak1090@gmail.com>
2025-09-21 21:43:35 +0530

    Feat:local setup

--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,12 @@
 // store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import { emptySplitApi } from "./rtk/emptySplitApi.ts";
- 
+import applicationReducer from "./slices/applicationSlice"
 export const store = configureStore({
   reducer: {
     [emptySplitApi.reducerPath]: emptySplitApi.reducer,
-    
+    application: applicationReducer,
+
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(emptySplitApi.middleware),
